f2461623292aa310f4944cd8ab9ed08cb3fbe56b
vipul-developer <vipul.sharma234@gmail.com>
2022-03-24 17:18:48 +0530

    Techeco Change Done Initiatives

--- src/Component/Initiatives/index.js
+++ src/Component/Initiatives/index.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import { Box,Grid,Container,Typography,CardMedia,Card,CardContent,Button } from "@mui/material";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css"; // This only needs to be imported once in your app
 import { Fade } from "react-awesome-reveal";
 import ContactForm from "../ContactUs/ContactForm";
 import Slider from "react-slick";
 import img1 from "../../Resources/Images/ini_img1.jpeg";
 import img2 from "../../Resources/Images/ini_img2.jpg";
 import img3 from "../../Resources/Images/ini_img3.jpg";
-
+import wallImage1 from "../../Resources/Images/Wall painiting/Techeco-Initiatives-wall-painting1.jpg";
+import wallImage2 from "../../Resources/Images/Wall painiting/Techeco-Initiatives-wall-painting2.jpeg";
+import wallImage3 from "../../Resources/Images/Wall painiting/Techeco-Initiatives-wall-painting3.jpeg";
+import safetyImage1 from "../../Resources/Images/Safety/Techeco-Initiatives-safety1.jpg";
+import safetyImage2 from "../../Resources/Images/Safety/Techeco-Initiatives-safety2.jpg";
+import safetyImage3 from "../../Resources/Images/Safety/Techeco-Initiatives-safety3.jpg";
+import safetyImage4 from "../../Resources/Images/Safety/Techeco-Initiatives-safety4.jpg";
+import safetyImage5 from "../../Resources/Images/Safety/Techeco-Initiatives-safety5.jpg";
+import safetyImage6 from "../../Resources/Images/Safety/Techeco-Initiatives-safety6.jpg";
+import safetyImage7 from "../../Resources/Images/Safety/Techeco-Initiatives-safety7.jpg";
+import safetyImage8 from "../../Resources/Images/Safety/Techeco-Initiatives-safety8.jpg";
+import mumbaiImage1 from "../../Resources/Images/Garware Institute Mumbai/Techeco-Initiatives-Garware2.jpg";
+import mumbaiImage2 from "../../Resources/Images/Garware Institute Mumbai/Techeco-Initiatives-Garware3.jpg";
+import mumbaiImage3 from "../../Resources/Images/Garware Institute Mumbai/Techeco-Initiatives-Garware5.jpg";
+import mumbaiImage4 from "../../Resources/Images/Garware Institute Mumbai/Techeco-Initiatives-Garware7.jpg";
+import collegeImage1 from "../../Resources/Images/NDMVP COLLEGE NASHIK/Techeco-Initiatives-Cans1.jpg";
+import collegeImage2 from "../../Resources/Images/NDMVP COLLEGE NASHIK/Techeco-Initiatives-Cans2.jpg";
+import collegeImage3 from "../../Resources/Images/NDMVP COLLEGE NASHIK/Techeco-Initiatives-Cans3.jpg";
+import collegeImage4 from "../../Resources/Images/NDMVP COLLEGE NASHIK/Techeco-Initiatives-Cans4.jpg";
+import collegeImage5 from "../../Resources/Images/NDMVP COLLEGE NASHIK/Techeco-Initiatives-Cans5.jpg";
+import collegeImage6 from "../../Resources/Images/NDMVP COLLEGE NASHIK/Techeco-Initiatives-Cans6.jpg";
+import collegeImage7 from "../../Resources/Images/NDMVP COLLEGE NASHIK/Techeco-Initiatives-Cans7.jpg";
 const Initiatives = () => {
+    const [ college, setCollege ] = React.useState(false);
+    const [ wall, setWall ] = React.useState(false);
+    const [ safety, setSafety ] = React.useState(false);
+    const [ mumbai, setMumbai ] = React.useState(false);
+    const [ photoIndex, setPhotoIndex ] = React.useState(0);
+    const collegeImages = [collegeImage1,collegeImage2,collegeImage3,collegeImage4,collegeImage5,collegeImage6,collegeImage7];
+    const wallImages = [wallImage1,wallImage2,wallImage3];
+    const safetyImages = [safetyImage1,safetyImage2,safetyImage3,safetyImage4,safetyImage5,safetyImage6,safetyImage7,safetyImage8];
+    const mumbaiImages = [mumbaiImage1,mumbaiImage2,mumbaiImage3,mumbaiImage4];
     const setting = {
         dots:true,
         infinite: true,
@@ -15,6 +47,22 @@ const Initiatives = () => {
         autoplay: true,
         slidesToShow: 1,
         slidesToScroll: 1,
+    }
+    const renderWallLight = (index) => {
+        setWall(true);
+        setPhotoIndex(index);
+    }
+    const renderCollegeLight = (index) => {
+        setCollege(true);
+        setPhotoIndex(index);
+    }
+    const renderSafetyLight = (index) => {
+        setSafety(true);
+        setPhotoIndex(index);
+    }
+    const renderMumbaiLight = (index) => {
+        setMumbai(true);
+        setPhotoIndex(index);
     }
     return (
         <>
@@ -91,7 +139,7 @@ const Initiatives = () => {
                                             </Typography>
                                         </Box>
                                         <Box sx={{mt:2}}>
-                                            <Button variant="outlined" sx={{border:"solid 1px #33bc82",padding:"12px 19px 11px 24px",borderRadius:"33px",fontSize:"14px",fontWeight:500,fontFamily:"Montserrat",color:"#33bc82",textTransform:"capitalize","&:hover":{border:"solid 1px #33bc82",backgroundColor:"transparent"},}}>know More</Button>
+                                            <Button variant="outlined" sx={{border:"solid 1px #33bc82",padding:"12px 19px 11px 24px",borderRadius:"33px",fontSize:"14px",fontWeight:500,fontFamily:"Montserrat",color:"#33bc82",textTransform:"capitalize","&:hover":{border:"solid 1px #33bc82",backgroundColor:"transparent"},}} onClick={() => renderWallLight(0)}>know More</Button>
                                         </Box>
                                     </CardContent>
                                 </Card>
@@ -112,7 +160,7 @@ const Initiatives = () => {
                                             </Typography>
                                         </Box>
                                         <Box sx={{mt:2}}>
-                                            <Button variant="outlined" sx={{border:"solid 1px #33bc82",padding:"12px 19px 11px 24px",borderRadius:"33px",fontSize:"14px",fontWeight:500,fontFamily:"Montserrat",color:"#33bc82",textTransform:"capitalize","&:hover":{border:"solid 1px #33bc82",backgroundColor:"transparent"},}}>know More</Button>
+                                            <Button variant="outlined" sx={{border:"solid 1px #33bc82",padding:"12px 19px 11px 24px",borderRadius:"33px",fontSize:"14px",fontWeight:500,fontFamily:"Montserrat",color:"#33bc82",textTransform:"capitalize","&:hover":{border:"solid 1px #33bc82",backgroundColor:"transparent"},}} onClick={() => renderSafetyLight(0)}>know More</Button>
                                         </Box>
                                     </CardContent>
                                 </Card>
@@ -133,7 +181,7 @@ const Initiatives = () => {
                                             </Typography>
                                         </Box>
                                         <Box sx={{mt:2}}>
-                                            <Button variant="outlined" sx={{border:"solid 1px #33bc82",padding:"12px 19px 11px 24px",borderRadius:"33px",fontSize:"14px",fontWeight:500,fontFamily:"Montserrat",color:"#33bc82",textTransform:"capitalize","&:hover":{border:"solid 1px #33bc82",backgroundColor:"transparent"},}}>know More</Button>
+                                            <Button variant="outlined" sx={{border:"solid 1px #33bc82",padding:"12px 19px 11px 24px",borderRadius:"33px",fontSize:"14px",fontWeight:500,fontFamily:"Montserrat",color:"#33bc82",textTransform:"capitalize","&:hover":{border:"solid 1px #33bc82",backgroundColor:"transparent"},}} onClick={() => renderMumbaiLight(0)}>know More</Button>
                                         </Box>
                                     </CardContent>
                                 </Card>
@@ -154,7 +202,7 @@ const Initiatives = () => {
                                             </Typography>
                                         </Box>
                                         <Box sx={{mt:2}}>
-                                            <Button variant="outlined" sx={{border:"solid 1px #33bc82",padding:"12px 19px 11px 24px",borderRadius:"33px",fontSize:"14px",fontWeight:500,fontFamily:"Montserrat",color:"#33bc82",textTransform:"capitalize","&:hover":{border:"solid 1px #33bc82",backgroundColor:"transparent"},}}>know More</Button>
+                                            <Button variant="outlined" sx={{border:"solid 1px #33bc82",padding:"12px 19px 11px 24px",borderRadius:"33px",fontSize:"14px",fontWeight:500,fontFamily:"Montserrat",color:"#33bc82",textTransform:"capitalize","&:hover":{border:"solid 1px #33bc82",backgroundColor:"transparent"},}} onClick={() => renderCollegeLight(0)}>know More</Button>
                                         </Box>
                                     </CardContent>
                                 </Card>
@@ -164,6 +212,58 @@ const Initiatives = () => {
                 </Container>    
             </Grid>
             <ContactForm/>
+            {
+                wall && (
+                    <Lightbox
+                        mainSrc={wallImages[photoIndex]}
+                        nextSrc={wallImages[(photoIndex + 1) % wallImages.length]}
+                        prevSrc={wallImages[(photoIndex + wallImages.length - 1) % wallImages.length]}
+                        onCloseRequest={() => setWall(false)}
+                        onMovePrevRequest={() => setPhotoIndex((photoIndex + wallImages.length - 1) % wallImages.length)}
+                        onMoveNextRequest={() => setPhotoIndex((photoIndex + 1) % wallImages.length)}
+                       
+                    />
+                )
+            }
+            {
+                college && (
+                    <Lightbox
+                        mainSrc={collegeImages[photoIndex]}
+                        nextSrc={collegeImages[(photoIndex + 1) % collegeImages.length]}
+                        prevSrc={collegeImages[(photoIndex + collegeImages.length - 1) % collegeImages.length]}
+                        onCloseRequest={() => setCollege(false)}
+                        onMovePrevRequest={() => setPhotoIndex((photoIndex + collegeImages.length - 1) % collegeImages.length)}
+                        onMoveNextRequest={() => setPhotoIndex((photoIndex + 1) % collegeImages.length)}
+                       
+                    />
+                )
+            }
+            {
+                mumbai && (
+                    <Lightbox
+                        mainSrc={mumbaiImages[photoIndex]}
+                        nextSrc={mumbaiImages[(photoIndex + 1) % mumbaiImages.length]}
+                        prevSrc={mumbaiImages[(photoIndex + mumbaiImages.length - 1) % mumbaiImages.length]}
+                        onCloseRequest={() => setMumbai(false)}
+                        onMovePrevRequest={() => setPhotoIndex((photoIndex + mumbaiImages.length - 1) % mumbaiImages.length)}
+                        onMoveNextRequest={() => setPhotoIndex((photoIndex + 1) % mumbaiImages.length)}
+                       
+                    />
+                )
+            }
+            {
+                safety && (
+                    <Lightbox
+                        mainSrc={safetyImages[photoIndex]}
+                        nextSrc={safetyImages[(photoIndex + 1) % safetyImages.length]}
+                        prevSrc={safetyImages[(photoIndex + safetyImages.length - 1) % safetyImages.length]}
+                        onCloseRequest={() => setSafety(false)}
+                        onMovePrevRequest={() => setPhotoIndex((photoIndex + safetyImages.length - 1) % safetyImages.length)}
+                        onMoveNextRequest={() => setPhotoIndex((photoIndex + 1) % safetyImages.length)}
+                       
+                    />
+                )
+            }
         </>
     );
 };
